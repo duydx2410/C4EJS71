5cc09e6bb7c42d307a00c980461707619e206d98
duy <duydx2410@gmail.com>
2020-02-22 21:17:25 +0700

    sửa bài

--- homework/exercise4.js
+++ homework/exercise4.js
@@ -75,47 +75,93 @@
 //Bài tập 5: nhập vào 3 số a, b, c. kiểm tra xem có lập được thành tam giác không, nếu được thì tam giá đó là tam giác gì:
 
 
-// let a = parseInt(prompt("Nhập vào giá trị của a:"));
-// let b = parseInt(prompt("Nhập vào giá trị cảu b:"));
-// let c = parseInt(prompt("Nhập vào giá trị của c:"));
-// if ((a+b>c) && (a+c>b) && (b+c>a)) {
-// console.log(`${a}, ${b}, ${c} Tạo được tam giác.`)
-// }
-// if ((a===b) || (b===c) || (a===c)) {
-// console.log("Và là tam giác cân.")
-// }
-// else if ((a===b) && (b===c) && (c===a)) {
-// console.log("Và là tam giác đều.")
-// }
-// else if ((a*a = b*b + c*c) || (b*b = a*a + c*c) || (c*c = a*a + b*b)) {
-// console.log("Và là tam giác vuông.")
+let a = parseInt(prompt("Nhập vào giá trị của a:"));
+let b = parseInt(prompt("Nhập vào giá trị cảu b:"));
+let c = parseInt(prompt("Nhập vào giá trị của c:"));
+if ((a+b>c) && (a+c>b) && (b+c>a)) {
+console.log(`${a}, ${b}, ${c} Tạo được tam giác.`)
+}
+if (((a===b) && (a!==c)) || ((b===c) && (a!==b)) || ((a===c) && (c!==a)) && ((a+b>c) && (a+c>b) && (b+c>a))) {
+console.log("Và là tam giác cân.")
+}
+else if ((a===b) && (b===c)) && ((a+b>c) && (a+c>b) && (b+c>a)) {
+console.log("Và là tam giác đều.")
+}
+else if ((a*a === b*b + c*c) || (b*b === a*a + c*c) || (c*c === a*a + b*b)) {
+console.log("Và là tam giác vuông.")
+}
+else {console.log("đây ko phải tam giác.")
+}
 
-// else {console.log("đây ko phải tam giác.")
-// }
+// //Bài tập 6: Cho thông tin bảng giá điện, nhập vào số điện sử dụng của 1 hộ trong 1 tháng, in ra số tiênf từng bậc và tổng số tiền phải trả.
+// let numberElectric = parseInt(prompt("Nhập vào số điện tiêu thụ trong tháng: (kWh)"));
+// let bac1max = 50 * 1678 ;
+// let bac2max = 50 * 1734 ;
+// let bac3max = 100 * 2014 ;
+// let bac4max = 100 * 2536 ;
+// let bac5max = 100 * 2834 ;
 
+// if ((0<numberElectric) && (numberElectric<=50)) {
+//     console.log(` Số tiền điện bậc 1 là: ${numberElectric*1678} vnđ
+//     Tổng tiền điện là: ${numberElectric*1678} vnđ`)  
+// };
+// if ((51<=numberElectric) && (numberElectric<=100)) {
+//     console.log(` Số tiền điện bậc 1 là: ${bac1max} vnđ 
+//  Số tiền điện bậc 2 là: ${(numberElectric-50)*1734} vnđ
+//  Tổng tiền điện là: ${bac1max+((numberElectric-50)*1734)} vnđ` )
+// };
+// if ((101<=numberElectric) && (numberElectric<=200)) {
+//     console.log(` Số tiền điện bậc 1 là: ${bac1max} vnđ
+//  Số tiền điện bậc 2 là: ${bac2max} vnđ
+//  Số tiền điện bậc 3 là: ${(numberElectric-100)*2014} vnđ
+//  Tổng tiền điện là: ${bac1max+bac2max+((numberElectric-100)*2014)} vnđ`)
+// };
+// if ((201<=numberElectric) && (numberElectric<=300)) {
+//     console.log(` Số tiền điện bậc 1 là: ${bac1max} vnđ
+//  Số tiền điện bậc 2 là: ${bac2max} vnđ
+//  Số tiền điện bậc 3 là: ${bac3max} vnđ
+//  Số tiền điện bậc 4 là: ${(numberElectric-200)*2536} vnđ
+//  Tổng tiền điện là: ${bac1max+bac2max+bac3max+((numberElectric-200)*2536)} vnđ`)   
+// };
+// if ((301<=numberElectric) && (numberElectric<=400)) {
+//     console.log(` Số tiền điện bậc 1 là: ${bac1max} vnđ
+//  Số tiền điện bậc 2 là: ${bac2max} vnđ
+//  Số tiền điện bậc 3 là: ${bac3max} vnđ
+//  Số tiền điện bậc 4 là: ${bac4max} vnđ
+//  Số tiền điện bậc 5 là: ${(numberElectric-300)*2834} vnđ
+//  Tổng tiền điện là: ${bac1max+bac2max+bac3max+bac4max+((numberElectric-300)*2834)} vnđ`)
+// };
+// if (401<=numberElectric) {
+//     console.log(` Số tiền điện bậc 1 là: ${bac1max} vnđ
+//  Số tiền điện bậc 2 là: ${bac2max} vnđ
+//  Số tiền điện bậc 3 là: ${bac3max} vnđ
+//  Số tiền điện bậc 4 là: ${bac4max} vnđ
+//  Số tiền điện bậc 5 là: ${bac5max} vnđ
+//  Số tiền điện bậc 6 là: ${(numberElectric-400)*2927} vnđ
+//  Tổng tiền điện là: ${bac1max+bac2max+bac3max+bac4max+bac5max+((numberElectric-400)*2927)} vnđ`)
+// };
 
 //Bài tập 7: Nhập vào chiều cao, cân nặng. tính chỉ số BMI, và đưa ra tình trạng theo cấp độ.
 
-let hight = parseFloat(prompt("Nhập vào chiều cao của bạn: m"));
-let weigth = parseFloat(prompt("Nhập vào cân nặng của bạn: kg"));
-let BMI = (weigth/(hight*hight));
-if (BMI < 18.5) {
-    console.log(` Chỉ số BMI của bạn là ${BMI}, bạn đang gầy.`)
-}
-else if ((18.5<BMI) && (BMI<24.9)) {
-    console.log(` Chỉ số BMI của bạn là ${BMI}, bạn đang bình thường.`)
-}
-else if ((25<BMI) && (BMI<29.9)) {
-    console.log(` Chỉ số BMI của bạn là ${BMI}, bạn đang hơi béo.`)
-}
-else if ((30<BMI) && (BMI<34.9)) {
-    console.log(` Chỉ số BMI của bạn là ${BMI}, Bạn đang béo phì cấp độ 1.`)
-}
-else if ((35<BMI) && (BMI<39.9)) {
-    console.log(` Chỉ số BMI của bạn là ${BMI}, Bận đang béo phì cấp độ 2.`)
-}
-else {(BMI>40);
-console.log(` Chỉ số BMI của bạn là ${BMI} WARNING: Bạn đã quá béo phì.`)
- }
+// let hight = parseFloat(prompt("Nhập vào chiều cao của bạn: m"));
+// let weigth = parseFloat(prompt("Nhập vào cân nặng của bạn: kg"));
+// let BMI = (weigth/(hight*hight));
+// if (BMI < 18.5) {
+//     console.log(` Chỉ số BMI của bạn là ${BMI}, bạn đang gầy.`)
+// }
+// else if (BMI<=24.9) {
+//     console.log(` Chỉ số BMI của bạn là ${BMI}, bạn đang bình thường.`)
+// }
+// else if (BMI<=29.9) {
+//     console.log(` Chỉ số BMI của bạn là ${BMI}, bạn đang hơi béo.`)
+// }
+// else if (BMI<=34.9) {
+//     console.log(` Chỉ số BMI của bạn là ${BMI}, Bạn đang béo phì cấp độ 1.`)
+// }
+// else if (BMI<=39.9) {
+//     console.log(` Chỉ số BMI của bạn là ${BMI}, Bận đang béo phì cấp độ 2.`)
+// }
+// else {console.log(` Chỉ số BMI của bạn là ${BMI} WARNING: Bạn đã quá béo phì.`)
+//  }
 
 //Bài tập 8
