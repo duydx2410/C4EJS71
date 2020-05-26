00a3d2b7b7ef5c4031a23fffb056972643fc71cd
duy <duydx01720@gmail.com>
2020-05-26 18:49:36 +0700

    update

--- session2_var/Script.js
+++ session2_var/Script.js
@@ -15,16 +15,18 @@
 // console.log(age);// cho code chạy tiếp
 // console.log(age1);
 
+/*
+var name;
+const PI = 3.14;
 
-// var name;
-// const PI = 3.14;
+let a = 5;
+let b = 10;
 
-// let a = 5;
-// let b = 10;
+let c = a + b; alert(c);
+console.log(c);
+//-
 
-// let c = a + b; alert(c);
-// console.log(c);
-// //-*/
+*/
 
 // let d = a - b;
 // console.log(d);
@@ -57,6 +59,61 @@
 
 // let c = a + b;
 // alert(c);
+
+
+//1: Nhập bán kính hình tròn, in ra chu vi, diện tính
+// let r = parseInt(prompt("Nhập bán kính hình tròn:"));
+// let p =  (2 * r) * Math.PI ;
+// let s = (r*r) * Math.PI ;
+// console.log(` Chu vi hình tròn bán kính ${r} là : ${p}`);
+// console.log(` Diện tính hình tròn bán kính ${r} là : ${s}`);
+
+//2: Nhập vào chiều dài, rộng, in ra chu vi, diện tính hình chữ nhật:
+
+// let a = parseInt(prompt("Nhập vào chiều rộng hình chữ nhật:"));
+// let b = parseInt(prompt("Nhập vào chiều dài hình chữ nhật:"));
+// let p = 2 * (a + b) ;
+// let s = a * b ;
+// console.log(` Chu vi hình chữ nhật là: ${p}`);
+// console.log(` Diện tích hình chữ nhật là: ${s}`);
+
+//3: Tính gia trị biểu thức:
+//a:
+
+// let x = parseInt(prompt("Nhập giá trị của X:"));
+// let y1 = 4 * ( x** + 10 * x * Math.sqrt(x) + 3 * x + 1);
+// console.log(` Tổng Y1 là: ${y1}`);
+
+//b:
+
+// let x = parseInt(prompt("Nhập giá trị của X:"));
+// let y2 = (Math.sin(Math.PI * Math.pow(x,2)) + (Math.sqrt(Math.pow(x,2) + 1))) / (Math.pow(Math.E,2*x) + Math.cos((Math.PI * x) / 4)) ;
+// console.log(` Tổng Y2 : ${y2}`);
+
+//4: Nhập số tiền X, đổi xem đc bao nhiêu tờ tiền các loại:
+
+// let money = parseInt(prompt("Nhập vào mệnh giá tiền:"));
+// let s1 = parseInt(money / 1e5) ;
+// let s2 = parseInt((money - (s1 * 1e5)) /5e4) ;
+// let s3 = parseInt((money - (s1 * 1e5 + s2 * 5e4)) / 2e4) ;
+// let s4 = (money - ( s1 * 1e5 + s2 * 5e4 + s3 * 2e4)) / 1e4 ;
+
+// console.log(` Số tiền ${money} đổi được ${s1} tờ 100000đ, ${s2} tờ 50000đ, ${s3} tờ 20000đ, ${s4} tờ 10000đ`);
+
+//5: Nhập 1 số có 3 chữ số, tính tổng các chữ số đó
+
+// let a = parseInt(prompt("Nhập số có 3 chữ số:"));
+// let s1 = parseInt(a / 100);
+// let s2 = parseInt((a - s1 * 100) / 10);
+// let s3 = parseInt(a - (s1 * 100 + s2 * 10)) ;
+// let s = s1 +s2 + s3 ;
+// console.log(a);
+// console.log(` Tổng 3 số ${s1} , ${s2} , ${s3}  là: ${s}`);
+
+
+
+
+
 
 
 
